3ebd1faf8b7a99d4740eaa1abaaf012c43844ba9
xiaoyaoke <15688169033@163.com>
2021-09-26 19:00:22 +0800

    github第一次提交

--- src/main/java/com/atgui/git/GitTest.java
+++ src/main/java/com/atgui/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("master git2");
         System.out.println("hot-fix git2");
+        System.out.println("yuancheng推送 github git2");
     }
 }
